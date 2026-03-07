efdb30a42a72ba2a79788bf58ceab5eefba5db23
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-03-07 14:28:37 +0900

    refactor(ts): prepare two apollo clients for both client-side and server-side

--- ts/lib/apollo-client.ts
+++ ts/lib/apollo-client.ts
@@ -1,38 +1,19 @@
+// Apollo client for server-side queries and mutations.
+
 import { HttpLink } from "@apollo/client"
-import { ApolloClient, InMemoryCache } from "@apollo/client-integration-nextjs"
+import {
+  ApolloClient,
+  InMemoryCache,
+  registerApolloClient,
+} from "@apollo/client-integration-nextjs"
 
 const link = new HttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:8080/query",
 })
 
-export function makeClient() {
+export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     link: link,
-    cache: new InMemoryCache({
-      typePolicies: {
-        Query: {
-          fields: {
-            transactions: {
-              keyArgs: ["startDate"],
-              merge(existing, incoming) {
-                return {
-                  ...incoming,
-                  nodes: [...(existing?.nodes ?? []), ...(incoming?.nodes ?? [])],
-                }
-              },
-            },
-            ledgerAccounts: {
-              keyArgs: ["kind"],
-              merge(existing, incoming) {
-                return {
-                  ...incoming,
-                  nodes: [...(existing?.nodes ?? []), ...(incoming?.nodes ?? [])],
-                }
-              },
-            },
-          },
-        },
-      },
-    }),
+    cache: new InMemoryCache(), // TODO: Set up cache policies and type policies as needed
   })
-}
+})
--- ts/lib/apollo-wrapper.tsx
+++ ts/lib/apollo-wrapper.tsx
@@ -1,7 +1,24 @@
 "use client"
 
-import { ApolloNextAppProvider } from "@apollo/client-integration-nextjs"
-import { makeClient } from "./apollo-client"
+// Apollo client for client-side queries and mutations.
+
+import { HttpLink } from "@apollo/client"
+import {
+  ApolloClient,
+  ApolloNextAppProvider,
+  InMemoryCache,
+} from "@apollo/client-integration-nextjs"
+
+const link = new HttpLink({
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:8080/query",
+})
+
+function makeClient() {
+  return new ApolloClient({
+    link: link,
+    cache: new InMemoryCache(), // TODO: Set up cache policies and type policies as needed
+  })
+}
 
 export function ApolloWrapper({ children }: { children: React.ReactNode }) {
   return <ApolloNextAppProvider makeClient={makeClient}>{children}</ApolloNextAppProvider>
