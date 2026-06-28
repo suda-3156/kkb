71bcd29f868bcdd831f140896f0b22220690b082
suda-3156 <116961611+suda-3156@users.noreply.github.com>
2026-06-28 22:10:40 +0900

    fix: nextjs component error

--- ts/app/layout.tsx
+++ ts/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="ja">
+    <html lang="ja" suppressHydrationWarning>
       <body
         className={cn(
           "font-shippori-mincho antialiased",
--- ts/components/theme-provider.tsx
+++ ts/components/theme-provider.tsx
@@ -1,15 +1,7 @@
 "use client"
 
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
-import { useEffect, useState } from "react"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  const [mounted, setMounted] = useState<boolean>(false)
-
-  useEffect(() => {
-    setMounted(true)
-    return () => setMounted(false)
-  }, [])
-
-  return mounted && <NextThemesProvider {...props}>{children}</NextThemesProvider>
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
